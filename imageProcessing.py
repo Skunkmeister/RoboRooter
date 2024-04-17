dff65dae0d04ed341a70e1f773268904f11ee39a
spotenza2016 <64374401+spotenza2016@users.noreply.github.com>
2024-04-17 08:13:46 -0400

    minor changes

--- imageProcessing.py
+++ imageProcessing.py
@@ -1,28 +1,33 @@
 import os
 os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 import cv2
-import numpy as np
+import cupy as np
+import time
 from ultralytics import YOLO
 
 def detectObjects(img2D, model, decreaseByProbability, confRequirement):
-    results = model.predict(source=img2D, conf=confRequirement, show_labels=False, save=False)
+    results = model.predict(source=img2D.get(), conf=confRequirement, show_labels=False, save=False, device='cuda:0')
     costmap = np.ones((img2D.shape[0], img2D.shape[1]))
     
     numberOfBoxes = results[0].boxes.shape[0]
-    boxes = results[0].boxes.numpy()
+    boxes = results[0].boxes.cpu().numpy()
     probabilities = boxes.conf
 
     if results[0].masks is None:
         return costmap
-    
+
     correctedBoxes = boxes.xyxy / results[0].orig_shape[1] * results[0].masks.shape[2]
-    probMasks = np.multiply(results[0].masks.data.numpy()[:].transpose(), probabilities[:] if decreaseByProbability else 0)
+    if decreaseByProbability:
+        probMasks = np.multiply(np.array(results[0].masks.data.cpu().numpy()).transpose(), np.array(probabilities))
+    else:
+        probMasks = np.array(results[0].masks.data.cpu().numpy()).transpose()
+
     sumMasks = np.sum(probMasks.transpose(), axis=0)
 
-    costmap = np.clip(costmap - cv2.resize(sumMasks, (img2D.shape[1], img2D.shape[0])), 0, 1)
+    costmap = np.clip(costmap - np.array(cv2.resize(np.asnumpy(sumMasks), (img2D.shape[1], img2D.shape[0]))), 0, 1)
                 
     return costmap
-    
+
 def depthToWorld(focalLength, cameraXYZ, cameraEuler, depthImage, colorImage):
     # Image dimensions
     [imgHeight, imgWidth] = depthImage.shape[0:2]
@@ -34,20 +39,20 @@ def depthToWorld(focalLength, cameraXYZ, cameraEuler, depthImage, colorImage):
     ry = cameraEuler[1]
     rz = cameraEuler[2]
     
-    Rx = np.matrix([[1, 0, 0], [0, np.cos(rx), -np.sin(rx)], [0, np.sin(rx), np.cos(rx)]])
-    Ry = np.matrix([[np.cos(ry), 0, np.sin(ry)], [0, 1, 0], [-np.sin(ry), 0, np.cos(ry)]])
-    Rz = np.matrix([[np.cos(rz), -np.sin(rz), 0], [np.sin(rz), np.cos(rz), 0], [0, 0, 1]])
+    Rx = np.array([[1, 0, 0], [0, np.cos(rx).get(), -np.sin(rx).get()], [0, np.sin(rx).get(), np.cos(rx).get()]])
+    Ry = np.array([[np.cos(ry).get(), 0, np.sin(ry).get()], [0, 1, 0], [-np.sin(ry).get(), 0, np.cos(ry).get()]])
+    Rz = np.array([[np.cos(rz).get(), -np.sin(rz).get(), 0], [np.sin(rz).get(), np.cos(rz).get(), 0], [0, 0, 1]])
     
     # The final rotation matrix is Rz * Ry * Rx
     rotationMatrix = Rx
-    rotationMatrix = rotationMatrix * Ry
-    rotationMatrix = rotationMatrix * Rz
+    rotationMatrix = np.matmul(rotationMatrix, Ry)
+    rotationMatrix = np.matmul(rotationMatrix, Rz)
     invRt = np.linalg.inv(rotationMatrix)
     
     # Calculate intrinsic matrix based on focal length and image dimensions
     cx = (imgWidth - 1) / 2  # Assuming the principal point is at the center
     cy = (imgHeight - 1) / 2 # Assuming the principal point is at the center
-    intrinsicMatrix = np.matrix([[focalLength, 0, cx],
+    intrinsicMatrix = np.array([[focalLength, 0, cx],
                        [0, focalLength, cy],
                        [0, 0, 1]])
     
@@ -64,8 +69,8 @@ def depthToWorld(focalLength, cameraXYZ, cameraEuler, depthImage, colorImage):
     
     coords = np.multiply(coords, depthImage)
     
-    coords = invIntrinsicMatrix * coords.transpose()
-    coords = invRt * coords
+    coords = np.matmul(invIntrinsicMatrix, coords.transpose())
+    coords = np.matmul(invRt, coords)
     
     coords = coords.transpose() + cameraXYZ
     
@@ -77,30 +82,30 @@ def depthToWorld(focalLength, cameraXYZ, cameraEuler, depthImage, colorImage):
     
     return result
     
-#mapPoint format is [y][x][b, g, r, weight]
 def flatten(xSize, ySize, scale, points, mapPoints):
     xOffset = xSize / 2
     yOffset = ySize / 2
     coords = np.stack((points[:, 2] * scale + yOffset, points[:, 0] * scale + xOffset))
 
-    yMin = max(np.min((coords[0, :]).astype(int)), 0)
-    yMax = min(np.max((coords[0, :]).astype(int)), ySize)
+    yMin = max(np.min((coords[0, :]).astype(int)).item(), 0)
+    yMax = min(np.max((coords[0, :]).astype(int)).item(), ySize)
     yFragSize = yMax - yMin
-    xMin = max(np.min((coords[1, :]).astype(int)), 0)
-    xMax = min(np.max((coords[1, :]).astype(int)), xSize)
+    xMin = max(np.min((coords[1, :]).astype(int)).item(), 0)
+    xMax = min(np.max((coords[1, :]).astype(int)).item(), xSize)
     xFragSize = xMax - xMin
 
     coords[0, :] -= yMin
     coords[1, :] -= xMin
-    
+
     abs_coords = np.ravel_multi_index(coords.astype(int), (yFragSize, xFragSize), mode = 'clip')
     b = np.bincount(abs_coords, weights = np.multiply(points[:, 3], points[:, 1]), minlength = yFragSize*xFragSize)
     g = np.bincount(abs_coords, weights = np.multiply(points[:, 4], points[:, 1]), minlength = yFragSize*xFragSize)
     r = np.bincount(abs_coords, weights = np.multiply(points[:, 5], points[:, 1]), minlength = yFragSize*xFragSize)
     c = np.bincount(abs_coords, weights = points[:, 1], minlength = yFragSize*xFragSize)
-    b = np.divide(b, c, where=c!=0)
-    g = np.divide(g, c, where=c!=0)
-    r = np.divide(r, c, where=c!=0)
+    c = np.where(c!=0, c, np.ones((c.shape)))
+    b = np.divide(b, c)
+    g = np.divide(g, c)
+    r = np.divide(r, c)
     img = np.dstack((b, g, r))
     img = img.reshape(yFragSize, xFragSize, 3)
     mapPoints = np.flip(mapPoints, 0)
@@ -128,16 +133,16 @@ def getImageChunk(environmentMap, costChunkXSize, costChunkYSize, chunkXOffset, 
     imageChunk = environmentMap[yMin:yMax, xMin:xMax]
     return imageChunk
 
-def updateCostMap(costMap, costChunk, chunkXOffset, chunkYOffset, position):
-    yMin = (position[1] - (costChunk.shape[0] / 2.0) + chunkYOffset).astype(int)
-    yMax = (yMin + costChunk.shape[0]).astype(int)
+def updateCostMap(costMap, costChunkXSize, costChunkYSize, costChunk, chunkXOffset, chunkYOffset, position):
+    yMin = (position[1] - (costChunkYSize / 2.0) + chunkYOffset).astype(int)
+    yMax = (yMin + costChunkYSize).astype(int)
 
     yMin = max(0, yMin)
     yMax = min(costMap.shape[0], yMax)
 
 
-    xMin = (position[0] - (costChunk.shape[1] / 2.0) + chunkXOffset).astype(int)
-    xMax = (xMin + costChunk.shape[1]).astype(int)
+    xMin = (position[0] - (costChunkXSize / 2.0) + chunkXOffset).astype(int)
+    xMax = (xMin + costChunkXSize).astype(int)
 
     xMin = max(0, xMin)
     xMax = min(costMap.shape[1], xMax)
@@ -147,16 +152,16 @@ def updateCostMap(costMap, costChunk, chunkXOffset, chunkYOffset, position):
 def main():
     # Settings:
     # Environment Map
-    mapXSize = 600
-    mapYSize = 800
+    mapXSize = 6000
+    mapYSize = 8000
     mapScale = 80
     # Generate point cloud
     focalLength = 50.0*1920/36.0
     # Flatten
     minimumWeight = 0.25
     # Update Costmap
-    costChunkXSize = 300
-    costChunkYSize = 200
+    costChunkXSize = 800
+    costChunkYSize = 600
     chunkXOffset = 0
     chunkYOffset = 0
     model = YOLO('yolov8x-seg.pt')
@@ -170,18 +175,20 @@ def main():
 
     running = True
     while(running):
+        time1 = time.time()
+        
         # TODO GET EKR DATA FOR POSITION AND ORIENTATION HERE, this is hardcoded to example 1 right now
-        cameraXYZ = -1 * np.matrix([5, 5, -2.5])
-        cameraXYZ[0, 1] = -1 * cameraXYZ[0, 1]
+        cameraXYZ = -1 * np.array([5, 5, -2.5])
+        cameraXYZ[1] = -1 * cameraXYZ[1]
         cameraEuler = [np.pi/4, 1.22173, 0] # Camera orientation as Euler angles (in radians)
-        posMat = np.matrix([300, 300, 0])
-        position = [posMat[0, 0], posMat[0, 1]]
+        posMat = np.array([mapXSize / 2, mapYSize / 2, 0])
+        position = [posMat[0], posMat[1]]
 
         # TODO GET DEPTH/COLOR SENSOR DATA, this is hard coded to example file right now
         depthImage = "../Depth.exr"
-        depthImageArray = cv2.imread(depthImage, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+        depthImageArray = np.array(cv2.imread(depthImage, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH))
         colorImage = "../Color.png"
-        colorImageArray = cv2.imread(colorImage)
+        colorImageArray = np.array(cv2.imread(colorImage))
         
     
         # Generate point cloud (may not be needed if depth camera gives automatically, even if so we should include the code but disable it)
@@ -199,14 +206,15 @@ def main():
         costChunk = detectObjects(imageChunk, model, decreaseByProbability, confRequirement)
 
         # Update the costmap
-        updateCostMap(costMap, costChunk, chunkXOffset, chunkYOffset, position)
+        updateCostMap(costMap, costChunkXSize, costChunkYSize, costChunk, chunkXOffset, chunkYOffset, position)
         
         # TODO PUBLISH ENVIRONMENT MAP AND COSTMAP TO ROS NODE FOR FRONT END
         
         # TODO UPDATE ROS COSTMAP HERE
 
+        time2 = time.time()
         
-        print('Loop Finished')
+        print('Loop Finished in ' + str((time2 - time1)*1000)[0:5] + ' ms...')
         
 if __name__ == "__main__":
     main()
